09c37af212a32398b65171441165597b8e0c2834
usernamekors <marek.kors@wp.pl>
2020-03-30 23:31:04 +0200

    offerts

--- components/sectionOffers/sectionOffers.js
+++ components/sectionOffers/sectionOffers.js
@@ -1,42 +1,46 @@
+// const btn = document.querySelector('.foto');
+// const hidden = document.querySelector('.wrapperSmallPic');
+
+// btn.addEventListener('click', () => {
+//     if (hidden.style.display === 'block') {
+//         hidden.style.display = 'none';
+//     } else {
+//         hidden.style.display = 'block';
+//     }
+// });
+// -----------------------------------------------------------------------------------------------------------
+
 // var accItem = document.getElementsByClassName('accordionItem');
 // var accHD = document.getElementsByClassName('accordionItemHeading');
+
 // for (i = 0; i < accHD.length; i++) {
-// 	accHD[i].addEventListener('click', toggleItem, false);
+//     accHD[i].addEventListener('click', toggleItem, false);
 // }
+
 // function toggleItem() {
-// 	var itemClass = this.parentNode.className;
-// 	for (i = 0; i < accItem.length; i++) {
-// 		accItem[i].className = 'accordionItem close';
-// 	}
-// 	if (itemClass == 'accordionItem close') {
-// 		this.parentNode.className = 'accordionItem open';
-// 	}
+//     var itemClass = this.parentNode.className;
+//     for (i = 0; i < accItem.length; i++) {
+//         accItem[i].className = 'accordionItem close';
+//     }
+//     if (itemClass == 'accordionItem close') {
+//         this.parentNode.className = 'accordionItem open';
+//     }
 // }
 // -----------------------------------------------------------------------------------------------------------
-// var accItem = document.querySelector('.open-project-item');
-// var accHD = document.querySelector('.project-item');
+// START #2
+// let acc = document.getElementsByClassName('foto');
+// let i;
 
-// for (i = 0; i < accHD.length; i++) {
-// 	accHD[i].addEventListener('click', toggleItem, false);
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener('click', function() {
+//         let panel = this.nextElementSibling;
+
+//         if (panel.style.display === 'block') {
+//             panel.style.display = 'none';
+//         } else {
+//             panel.style.display = 'block';
+//         }
+//     });
 // }
-
-// function toggleItem() {
-// 	var itemClass = document.querySelector('.project-item');
-// 	for (i = 0; i < accItem.length; i++) {
-// 		accItem[i].className = 'project-item';
-// 	}
-// 	if (itemClass == 'project-item') {
-// 		this.parentNode.className = 'open-project-item';
-// 	}
-// }
-// ------------------------------------------------------------------------------------------------------
-const btn = document.querySelector('.project-item');
-const hidden = document.querySelector('.open-project-item');
-
-btn.addEventListener('click', () => {
-	if (hidden.style.display === 'block') {
-		hidden.style.display = 'none';
-	} else {
-		hidden.style.display = 'block';
-	}
-});
+// END #2
+// ------------------------------------------------------------------------------------------------------}
--- components/sectionOrders/sectionOrders.js
+++ components/sectionOrders/sectionOrders.js
@@ -18,14 +18,15 @@
 var accItem = document.getElementsByClassName('accordionItem');
 var accHD = document.getElementsByClassName('accordionItemHeading');
 for (i = 0; i < accHD.length; i++) {
-	accHD[i].addEventListener('click', toggleItem, false);
+    accHD[i].addEventListener('click', toggleItem, false);
 }
+
 function toggleItem() {
-	var itemClass = this.parentNode.className;
-	for (i = 0; i < accItem.length; i++) {
-		accItem[i].className = 'accordionItem close';
-	}
-	if (itemClass == 'accordionItem close') {
-		this.parentNode.className = 'accordionItem open';
-	}
+    var itemClass = this.parentNode.className;
+    for (i = 0; i < accItem.length; i++) {
+        accItem[i].className = 'accordionItem close';
+    }
+    if (itemClass == 'accordionItem close') {
+        this.parentNode.className = 'accordionItem open';
+    }
 }
