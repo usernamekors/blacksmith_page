4ad8691885191f5e05ce554c3d5fde9174f5d01f
usernamekors <marek.kors@wp.pl>
2020-04-04 21:09:18 +0200

    add scroll

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -1,11 +1,22 @@
-document.querySelectorAll('nav ul li').forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        gsap.to(window, {
-            duration: 1,
+$(function() {
+    var wrapper = $('#main'),
+        $menu = $('#menu'),
+        $window = $(window);
+
+    $menu.on('click', 'a', function() {
+        var $this = $(this),
+            href = $this.attr('href'),
+            topY = $(href).offset().top;
+
+        TweenMax.to($window, 1, {
             scrollTo: {
-                y: '#section' + (index + 1),
-                offsetY: 80
-            }
+                y: topY,
+                offsetY: 80,
+                autoKill: true
+            },
+            ease: Power3.easeOut
         });
+
+        return false;
     });
 });
