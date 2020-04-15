6adef34aa2abef7a78bcefa8a844b227927260d8
usernamekors <marek.kors@wp.pl>
2020-04-15 07:56:49 +0200

    pure js hamb

--- components/nav/nav.js
+++ components/nav/nav.js
@@ -27,3 +27,21 @@ btn.addEventListener('click', () => {
     gsap.to(window, { duration: 1, scrollTo: { y: '#home', offsetY: 50 } });
 });
 
+let btn1 = document.querySelector('#btn1');
+let btn2 = document.querySelector('#btn2');
+
+btn1.addEventListener('click', () => {
+    if (btn1.style.display === 'block') {
+        btn2.style.display = 'none';
+    } else if ((btn1.style.display = 'none')) {
+        btn2.style.display = 'block';
+    }
+});
+
+btn2.addEventListener('click', () => {
+    if (btn2.style.display === 'none') {
+        btn1.style.display = 'block';
+    } else if ((btn2.style.display = 'none')) {
+        btn1.style.display = 'block';
+    }
+});
