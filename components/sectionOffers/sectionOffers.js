fc9e590090008eaaa3217bcd09b1fcb95fce8fba
orgasm69 <cichy.shebang@gmail.com>
2020-05-13 13:49:17 +0200

    dodane favicon, prawdziwe zdjecia, zmiana slidera, poprawa logo na lowres

--- components/sectionOffers/sectionOffers.js
+++ components/sectionOffers/sectionOffers.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.foto').forEach((btn) => {
     btn.addEventListener('click', () => {
         gsap.to(window, {
-            duration: 2.5,
+            duration: 0.5,
             scrollTo: { y: '#specjalSmallPic', offsetY: 80, autoKill: false },
             ease: 'power3'
         });
