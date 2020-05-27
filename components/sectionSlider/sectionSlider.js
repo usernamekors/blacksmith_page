93a14a023ea905455cfb580caa18dd644496d83a
usernamekors <marek.kors@wp.pl>
2020-05-27 17:29:23 +0200

    kill dots

--- components/sectionSlider/sectionSlider.js
+++ components/sectionSlider/sectionSlider.js
@@ -13,14 +13,8 @@ const slideList = [{
 ];
 const image = document.querySelector('img.slider');
 const h1 = document.querySelector('h1.slider');
-const dots = [...document.querySelectorAll('.dots span')]
 const time = 3000;
 let active = 0;
-const changeDot = () => {
-    const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
-    dots[activeDot].classList.remove('active');
-    dots[active].classList.add('active');
-}
 
 const changeSlide = () => {
     active++;
